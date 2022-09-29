4642190f25b85452f592e79bd58381b4bf74178a
Gabriela Landberg Llave <landberg@cs.ualberta.ca>
2022-09-28 19:30:33 -0600

    Adjust original code to log correct login and password info

--- login.py
+++ login.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python3
 import cgi
 import os
+import secret
 from templates import login_page
 from templates import secret_page
 
 def parse_cookies(cookie_string):
-    cookies = cookie_string.split(";")
     result = {}
+    if not cookie_string:
+        return result
+    cookies = cookie_string.split(";")
     for cookie in cookies:
         split_cookie = cookie.split("=")
         result[split_cookie[0]] = split_cookie[1]
@@ -23,11 +26,15 @@ password = form.getfirst("password")
 header = "Content-Type: text/html\r\n"
 
 body = ""
+correct_login = False
 
-if username is not None or ('logged' in cookies and cookies['logged'] == "true"):
+if (username == secret.username and password == secret.password):
+    correct_login = True
+    header += "Set-Cookie: correct_login=true\r\n"
+
+if correct_login or ('logged' in cookies and cookies['logged'] == "true"):
     body += secret_page(username, password)
-    header += "Set-Cookie: logged=true; Max-Age=60\r\n"
-    body+= "<h1>A terrible secret</h1>"
+    header += "Set-Cookie: logged=true\r\n"
 else:
     body += login_page()
 
